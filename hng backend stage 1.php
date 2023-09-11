755b70ced8bfbae197092870ce8d9faa414b5cc1
ikegwuonu <144610193+ikegwuonu@users.noreply.github.com>
2023-09-11 19:46:23 +0100

    Update hng backend stage 1.php
    new update to backend

--- hng backend stage 1.php
+++ hng backend stage 1.php
@@ -18,8 +18,8 @@ if (isset($_GET['slack_name'])) {
 	$me->current_day= date("l");
 	$me->utc_time=  gmdate("Y-m-d H:i:s ");
 	$me->track = $_GET['track'];
-	$me->github_file_url="j";
-	$me->github_repo_url="g";
+	$me->github_file_url="https://github.com/ikegwuonu/ikegwuonu.github.io/blob/main/hng%20backend%20stage%201.php";
+	$me->github_repo_url="https://github.com/ikegwuonu/ikegwuonu.github.io/tree/main";
 	$me->status_code=200;
 	$myJSON = json_encode($me);
 	
